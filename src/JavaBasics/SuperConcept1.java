eb474e640bc08ae5749990738b44dcf3e4120eec
Gowtham Kumar <ms.gowthamkumar99@gmail.com>
2020-04-23 15:00:29 +0200

    Changes in Constructor

--- src/JavaBasics/SuperConcept1.java
+++ src/JavaBasics/SuperConcept1.java
@@ -1,17 +1,25 @@
 package JavaBasics;
 
-public class SuperConcept1 extends SuperConcept{
-	
-	public SuperConcept1() {
-		
-		super(99,9);
-		
+public class SuperConcept1 extends SuperConcept {
+
+	public SuperConcept1(int i,int j) {
+
+		super(i,j);
+
 		System.out.println("Child Class Constructor");
 	}
-	
+	public SuperConcept1(int i) {
+
+		super(i);
+
+		System.out.println("Child Class Constructor");
+	}
+
 	public static void main(String args[]) {
+
+		SuperConcept1 obj = new SuperConcept1(9,99);
+		SuperConcept1 obj1 = new SuperConcept1(999);
 		
-		SuperConcept1 obj = new SuperConcept1();
 	}
 
 }
